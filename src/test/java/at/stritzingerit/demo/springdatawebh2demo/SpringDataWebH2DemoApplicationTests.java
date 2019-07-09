67693691d85d19fa2d0e5fa6fc87f465d6b9d8da
Peter Stritzinger <office@subit.at>
2019-07-09 17:37:58 +0200

    initial add start.spring.io generated proejct

---
+++ src/test/java/at/stritzingerit/demo/springdatawebh2demo/SpringDataWebH2DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package at.stritzingerit.demo.springdatawebh2demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringDataWebH2DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
